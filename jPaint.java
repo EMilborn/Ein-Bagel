7ccc95a8769217373c44d0fb79fadbd6c27cb32e
EMilborn <emilborn@stuy.edu>
2016-01-11 16:23:47 -0500

    added basic framework for class jPaint, added Tile.java which will possibly be removed later

---
+++ jPaint.java
@@ -0,0 +1,45 @@
+public class jPaint{
+    
+    /*****************
+     INSTANCE VARIABLES
+     *****************/
+    
+    private Tile[][] easel;
+    //We could switch to String array and not use any other files
+    
+    private int cursorX, cursorY;
+
+    private boolean cursorDown;
+
+    private String name;
+    //Used for name when you save it as a file 
+
+
+    /**************
+     *CONSTRUCTORS*
+     **************/
+    
+    public jPaint(){
+	this(16, 16, "test");
+    }
+
+    public jPaint(int height, int width,String newName){
+	easel =  new Tile[height][width];
+	name = newName;
+    }
+
+
+    /*********
+     *METHODS*
+     *********/
+    
+    
+    
+    /******
+     *MAIN*
+     ******/
+    
+    public static void main(String[] args){
+    }
+}
+
