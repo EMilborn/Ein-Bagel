927254fe9d98e1c173af9b4a4fc12d08850e46a1
71619997a <gmarks@stuy.edu>
2016-01-11 19:05:46 -0500

    Added mode, started on colors and selection, changed constructor to fit

--- jPaint.java
+++ jPaint.java
@@ -5,35 +5,77 @@ public class jPaint{
      *****************/
     
     private String[][] easel;
-    //We could switch to String array and not use any other files
     
     private int cursorX, cursorY;
 
     private boolean cursorDown;
 
-    private String name;
-    //Used for name when you save it as a file
+    private String mode;
 
+    private String color; //current color
+
+    //ANSI escape sequences
+    public static final String ANSI = "\033[";
+    
+    public static final String CLEAR = "\033c"; //special for reasons 
+
+    //BACKGROUND COLORS
+    public static final String BLACK = ANSI+"40m";
+    public static final String RED = ANSI+"41m";
+    public static final String GREEN = ANSI+"42m";
+    public static final String YELLOW = ANSI+"43m";
+    public static final String BLUE = ANSI+"44m";
+    public static final String PINK = ANSI+"45m";
+    public static final String CYAN = ANSI+"46m";
+    public static final String WHITE = ANSI+"47m";
+
+    //FOREGROUND COLORS
+    public static final String CBLACK = ANSI+"30m";
+    public static final String CWHITE = ANSI+"37m";
+    
+    public static final String RESET = ANSI+"0m";
 
     /**************
      *CONSTRUCTORS*
      **************/
     
     public jPaint(){
-	this(16, 16, "test");
+	this(16, 16);
     }
 
-    public jPaint(int height, int width,String newName){
+    public jPaint(int height, int width){
 	easel =  new String[height][width];
-	name = newName;
+	mode = "main";
+	color = WHITE;
+	
     }
 
 
     /*********
      *METHODS*
      *********/
-    
-    public void input (int i){
+
+    //Handles input to do certain things in certain modes, returns if input is valid
+    public boolean input (int i){
+	char key = (char) i;
+	if(key == ' ') System.exit(0); //stops program
+	if(mode.equals("main")) {
+	    if(key == 'c') {
+		mode = "color";
+		return true;
+	    }
+	}
+	else if(mode.equals("color")) {
+	    int num =  Character.getNumericValue(key); //if key is not a number, gives -1
+	    if(num == -1 || num > 7) { //0-7 selection
+		return false;
+	    }
+	    //else:
+	    color = ANSI + "4" + num + "m"; //sets the color to correct ANSI sequence
+	    mode = "main";
+	    return true;
+	}
+        return false;
     }
     
     
@@ -42,8 +84,8 @@ public class jPaint{
      ******/
     
     public static void main(String[] args){
-
-
+	jPaint inst = new jPaint();
+	System.out.println(CLEAR);
 	while(true) { //MAIN LOOP
 	    int keyCode = 0;
 	    try {
@@ -51,7 +93,17 @@ public class jPaint{
 	    } catch(Exception e) {
 		continue; //no input
 	    }
-	    System.out.println("\033c"); //clear
+	    if(!inst.input(keyCode)) continue; //input returned false, invalid input 
+	    System.out.println(CLEAR); //now start drawing stuff, clear first
+	    if(inst.mode == "main") {
+		String tColor = CWHITE;
+		if(inst.color.equals(WHITE)) tColor = CBLACK;
+	        System.out.println(inst.color + tColor + "O"); //temporary
+	    }
+	    
+	    else if(inst.mode == "color") {
+		System.out.println("COLOR MODE"); //also temp
+	    }
 
 	}
     }
